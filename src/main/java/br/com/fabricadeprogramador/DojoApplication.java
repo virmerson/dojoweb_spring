6526269699f2a091ec0b8f07af353c656b06a5f6
virmerson <virmerson@hightechcursos.com.br>
2016-08-05 23:00:30 -0400

    JSF sem Spring Security

--- src/main/java/br/com/fabricadeprogramador/DojoApplication.java
+++ src/main/java/br/com/fabricadeprogramador/DojoApplication.java
@@ -1,21 +1,59 @@
 package br.com.fabricadeprogramador;
 
+import javax.faces.webapp.FacesServlet;
+import javax.servlet.ServletContext;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ServletListenerRegistrationBean;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.context.ServletContextAware;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import com.sun.faces.config.ConfigureListener;
 
 @SpringBootApplication
-public class DojoApplication   extends SpringBootServletInitializer
-{
+public class DojoApplication   extends SpringBootServletInitializer implements ServletContextAware {
+    private static Log logger = LogFactory.getLog(DojoApplication.class);
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(DojoApplication.class);
+    public DojoApplication() {
+        logger.debug("Initialised Application");
     }
 
+    public static void main(String[] args) {
+        SpringApplication.run(DojoApplication.class);
+    }
+    
+    @Bean
+    public ServletListenerRegistrationBean<ConfigureListener> jsfConfigureListener() {
+        return new ServletListenerRegistrationBean<ConfigureListener>(
+                new ConfigureListener());
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(DojoApplication.class, args);
-	}
+    @Bean
+    public ServletRegistrationBean facesServletRegistration() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(
+                new FacesServlet(), "*.xhtml", "*.jsf");
+        registration.setLoadOnStartup(1);
+        return registration;
+    }
+
+    @Bean
+    public ViewResolver getViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsf");
+        return resolver;
+    }
+
+    @Override
+    public void setServletContext(ServletContext servletContext) {
+        servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
+    }
+    
 }
